6705053897a8853cd0877c2a24e9ea0c8ca38010
zito <zito@hddn.io>
2026-05-29 09:49:36 +0900

    Apply shared form page padding

--- react-app/src/components/layout/AppLayout.jsx
+++ react-app/src/components/layout/AppLayout.jsx
@@ -1,13 +1,23 @@
 import { Layout } from 'antd'
 import { useState } from 'react'
-import { Outlet } from 'react-router-dom'
+import { matchPath, Outlet, useLocation } from 'react-router-dom'
 import Sidebar from './Sidebar'
 import TopHeader from './TopHeader'
 
 const { Content } = Layout
+const FORM_PAGE_PATTERNS = [
+  '/repositories/new',
+  '/repositories/:repositoryId/merge-requests/new',
+  '/deployment-transfer/new',
+  '/repositories/:repositoryId/deployment-transfer/new',
+  '/repositories/:repositoryId/settings/policy-change-request',
+  '/repositories/:repositoryId/settings/policy-exception-request',
+]
 
 function AppLayout() {
   const [collapsed, setCollapsed] = useState(false)
+  const location = useLocation()
+  const isFormPage = FORM_PAGE_PATTERNS.some((pattern) => matchPath({ path: pattern, end: true }, location.pathname))
 
   return (
     <Layout className="app-shell">
@@ -17,7 +27,7 @@ function AppLayout() {
           collapsed={collapsed}
           onToggleSidebar={() => setCollapsed((value) => !value)}
         />
-        <Content className="page-content">
+        <Content className={`page-content ${isFormPage ? 'page-content--form' : ''}`.trim()}>
           <Outlet />
         </Content>
       </Layout>
